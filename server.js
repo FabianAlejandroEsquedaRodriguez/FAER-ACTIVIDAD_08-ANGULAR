0ca2d436f037435bd6e409717b83d77a1470f0de
Fabián Esqueda <fabian.esqueda2892@alumnos.udg.mx>
2022-03-22 20:12:47 -0600

    Aplicacion hecha con angular

---
+++ server.js
@@ -0,0 +1,13 @@
+/*Crear nuestro servidor*/
+const express = require('express');
+const path = require('path'); 
+
+const app = express(); 
+
+app.use(express.static('./dist/project'));
+
+app.get('/*', (req,res) =>
+        res.sendFile('index.html', {root: 'dist/project/'}),
+    );
+
+app.listen(process.env.PORT || 8080);
